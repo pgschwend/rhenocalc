b63053e725011448cdab228e8f4e9bb9c8cba76d
Patrick Gschwend <patrick.gschwend@veratron.com>
2026-05-26 11:45:57 +0200

    Icon implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <QApplication>
+#include <QIcon>
 #include "ui/mainwindow.h"
 
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/calculator.svg"));
     MainWindow w;
+    w.setWindowIcon(QIcon(":/icons/calculator.svg"));
     w.show();
     return QApplication::exec();
 }
